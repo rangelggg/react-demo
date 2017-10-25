ca4c82edd6d001d2f1ca2c99d96cfd614cd20e8c
rangel <ruguo@thoughtworks.com>
2017-10-25 22:31:35 +0530

    [Ru] | add hello-world demo

---
+++ view/Hello-world.jsx
@@ -0,0 +1,12 @@
+class App extends React.Component {
+
+    render() {
+        return <div>
+            Hello world!
+        </div>;
+    }
+}
+
+ReactDOM.render(
+    <App/>, document.getElementById('content')
+);
